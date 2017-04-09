e940e2a7a6782af403935458753743f0b9e120e1
Kenny-Rogers <ylodonu@gmail.com>
2017-04-09 22:14:37 +0000

    changing file name

---
+++ site/pages/register.php
@@ -0,0 +1,66 @@
+<div class="bg1">
+  <div class="main">
+    <article id="content"><div class="ic">More Website Templates @ TemplateMonster.com - November 14, 2011!</div>
+      <div class="wrapper">
+        <div class="col2 marg_right1">
+          <h2>Who We Are</h2>
+          <div class="wrapper">
+            <figure class="left marg_right1"><img src="images/page2_img1.jpg" alt=""></figure>
+            <p class="color2 pad_bot1">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium</p>
+            <p>Corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi.</p>
+          </div>
+          <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minusi quod maxime placeat facere possimus omnis voluptas assumenda est omnis dolor repellendus. Temporibus autem quibusdam aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates.</p>
+          <a href="#" class="button1">Read More</a>
+        </div>
+        <div class="col1">
+          <h2>Why Choose Us</h2>
+          <div class="wrapper">
+            <span class="dropcap_1">1</span>
+            <p class="pad_bot1"><span class="color1">Quis auteure reprehenderit</span><br>
+                Dalesuad asorbi nunra vidasea atcurs usecuctu.</p>
+          </div>
+          <div class="wrapper">
+            <span class="dropcap_1">2</span>
+            <p class="pad_bot1"><span class="color1">Neque porro quisquam estorem</span><br>
+                Naser maellus pore qus esedase- se dusce.</p>
+          </div>
+          <div class="wrapper">
+            <span class="dropcap_1">3</span>
+            <p class="pad_bot1"><span class="color1">Nemo enim voluptatem</span><br>
+                Atcurs usuctu aecenas ttique  maellrase qus esedasese.</p>
+          </div>
+          <div class="wrapper">
+            <span class="dropcap_1">4</span>
+            <p class="pad_bot1"><span class="color1">Sed ut perspiciatis unde omnis</span><br>
+                Merts maellrase qus esedias dusceer lrurtasfeugiat.</p>
+          </div>
+        </div>
+      </div>
+    </article>
+    <article id="content2">
+      <div class="wrapper">
+        <h3>Our History</h3>
+        <div class="col1 marg_right1">
+          <h3 class="color1">1999</h3>
+          <p>Corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum.</p>
+          <a href="#" class="button1">Read More</a>
+        </div>
+        <div class="col1 marg_right1">
+          <h3 class="color1">2004</h3>
+          <p>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat mus.</p>
+          <a href="#" class="button1">Read More</a>
+        </div>
+        <div class="col1 marg_right1">
+          <h3 class="color1">2007</h3>
+          <p>Maxime placeat facere possi- mus omnis voluptas assumen- da est omnis dolor repellendus. Temporibus autem quibusdam aut officiis debitis aut rerum necessitatibus saepe.</p>
+          <a href="#" class="button1">Read More</a>
+        </div>
+        <div class="col1">
+          <h3 class="color1">2011</h3>
+          <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minusi quod maxime placeat facere.</p>
+          <a href="#" class="button1">Read More</a>
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
