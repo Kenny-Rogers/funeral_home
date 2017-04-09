55944f68696c62c5cb121aab11f73f7483a208b0
Kenny-Rogers <ylodonu@gmail.com>
2017-04-09 21:25:05 +0000

    public website

---
+++ site/components/navbar.php
@@ -0,0 +1,21 @@
+<div class="main">
+  <header>
+    <div class="wrapper">
+      <h1><a href="index.html" id="logo">Superior.com</a></h1>
+      <form id="search" method="post">
+        <fieldset>
+          <div class="bg"><input class="input" type="text" value="Search"  onblur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''" ></div>
+        </fieldset>
+      </form>
+    </div>
+    <nav>
+      <ul id="menu">
+        <li><a href="?page=home"><span>Home</span></a></li>
+        <li class="active"><a href="?page=company"><span>Company</span></a></li>
+        <li><a href="?page=solution"><span>Solutions</span></a></li>
+        <li><a href="?page=service"><span>Services</span></a></li>
+        <li class="last"><a href="?page=contact"><span>Contacts</span></a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
---
+++ site/index.php
@@ -0,0 +1,15 @@
+<?php include('../includes/page_loader.php'); ?>
+<!-- head -->
+  <?php include('components/header.php');?>
+
+<!--header -->
+  <?php include('components/navbar.php'); ?>
+<!--header end-->
+
+<!--content -->
+  <?php include($display); ?>
+<!--content end-->
+
+<!--footer -->
+  <?php include('components/footer.php'); ?>
+<!--footer end-->
