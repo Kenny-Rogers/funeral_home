849b0521e7c467e46b475cb84d15084f56de2467
ikeamofa <ikeamofa14@gmail.com>
2017-04-09 23:13:57 -0700

    displays lists of services performed

---
+++ site/pages/services.php
@@ -0,0 +1,49 @@
+<div class="bg1">
+  <div class="main">
+<article id="content"><div class="ic"></div>
+  <div class="wrapper">
+    <h2>Services Overview</h2>
+    <div class="col1 marg_right1">
+      <div class="wrapper">
+        <figure class="left marg_right2"><img src="images/page4_img1.jpg" alt=""></figure>
+        <p><span class="color1">Dressing</span><br>
+          Non provident, similique sunt in culpa qui officia deserunt mollitia animi. Et harum quidem rerum facilis.</p>
+      </div>
+      <div class="wrapper">
+        <figure class="left marg_right2"><img src="images/page4_img2.jpg" alt=""></figure>
+        <p><span class="color1">Casketting</span><br>
+            Cam libero tempore cum soluta nobis est eligendi cumque nihil impedit quo minusi quod maxime placeat </p>
+      </div>
+    </div>
+    <div class="col1 marg_right1">
+      <div class="wrapper">
+        <figure class="left marg_right2"><img src="images/page4_img3.jpg" alt=""></figure>
+        <p><span class="color1">Cossetting(apllication of cosmetics and embalment)</span><br>
+          Non provident, similique sunt in culpa qui officia deserunt mollitia animi. Et harum quidem rerum facilis.</p>
+      </div>
+      <div class="wrapper">
+        <figure class="left marg_right2"><img src="images/page4_img4.jpg" alt=""></figure>
+        <p><span class="color1">Washing</span><br>
+            Cam libero tempore cum soluta nobis est eligendi cumque nihil impedit quo minusi quod maxime placeat </p>
+      </div>
+    </div>
+    <div class="col1">
+      <div class="wrapper">
+        <figure class="left marg_right2"><img src="images/page4_img5.jpg" alt=""></figure>
+        <p><span class="color1">Cremation</span><br>
+          Non provident, similique sunt in culpa qui officia deserunt mollitia animi. Et harum quidem rerum facilis.</p>
+      </div>
+      <div class="wrapper">
+        <figure class="left marg_right2"><img src="images/page4_img6.jpg" alt=""></figure>
+        <p><span class="color1">Transportation of dead bodies</span><br>
+            Cam libero tempore cum soluta nobis est eligendi cumque nihil impedit quo minusi quod maxime placeat </p>
+      </div>
+      <div class="col1 marg_right1">
+      </div>
+
+    </div>
+  </div>
+</article>
+
+</div >
+</div >
