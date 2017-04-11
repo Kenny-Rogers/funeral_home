d22041b392ff2a89c27b6aca7066c4f918670f15
Kenny-Rogers <ylodonu@gmail.com>
2017-04-11 11:37:59 +0000

    index page including all the various sections

---
+++ admin/index.php
@@ -0,0 +1,9 @@
+<?php include("components/head.php"); ?>
+
+<?php include("components/header.php"); ?>
+
+<?php include("components/menu.php"); ?>
+
+<?php include("components/main_page.php"); ?>
+
+<?php include("components/footer.php"); ?>
