665a0c34117fb62a0ed349191068835fc3f682a1
Kenny-Rogers <ylodonu@gmail.com>
2017-04-11 22:54:28 +0000

    contains all variables used in the public website initialised to ""

---
+++ includes/public/init_vars.php
@@ -0,0 +1,15 @@
+<?php
+//include all the variables used in the site folder
+
+//LOCATION:: includes/public/page_loader.php
+//PURPOSE:: store the requested page
+$page = "";
+
+//LOCATION:: site/components/nav_bar.php
+//PURPOSE::  makes the clicked link active on click
+$class_s = "";
+$class_o = "";
+$class_a = "";
+$class_l = "";
+$class_h = "";
+?>
