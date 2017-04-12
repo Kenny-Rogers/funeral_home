6d19d7a17cc384391dc192a4138a8f4ccba3813b
Kenny-Rogers <ylodonu@gmail.com>
2017-04-12 13:37:54 +0000

    adding display variable from page_loader.php

--- includes/public/init_vars.php
+++ includes/public/init_vars.php
@@ -5,6 +5,7 @@
 //LOCATION:: includes/public/page_loader.php
 //PURPOSE:: store the requested page
 $page = "";
+$display = "";
 
 //LOCATION:: site/components/nav_bar.php
 //PURPOSE::  makes the clicked link active on click
