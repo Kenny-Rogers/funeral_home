4c674e1e31ce6faf37df2b51d533bdf5be7e8bc2
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 02:45:58 +0000

    adding the used variables

--- includes/admin/init_vars.php
+++ includes/admin/init_vars.php
@@ -1,5 +1,8 @@
 <?php
 //contains all variables used in the project initialised to a default value;
-$full_name = "";    //used in admin/components/header.php
-
+$full_name = "";    //used in public/admin/components/header.php
+$user = "";         //used in includes/admin/user_loader.php
+$page_loader = "";  //used in includes/admin/user_loader.php
+$menu = "";         //used in includes/admin/user_loader.php
+$display = "";      //used in includes/admin/page_loaders/*
 ?>
