3d61da48eee088567e4b2c378e5214cd8a99686c
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 12:54:29 +0000

    remove staff from system

---
+++ public/admin/components/pages/admin/remove_staff.php
@@ -0,0 +1,38 @@
+<div class="row">
+        <div class="col-xs-12">
+          <div class="box box-primary">
+            <!--div class="box-header">
+              <h3 class="box-title"></h3>
+            </div-->
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Staff Number</th>
+                  <th>Name</th>
+                  <th>Role</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                  <td>Misc</td>
+                  <td>PSP browser</td>
+                  <td>PSP</td>
+                  <td><a href="?page=delete_staff&worker_id=4">Remove</a></td>
+                </tr>
+                <tr>
+                  <td>Other browsers</td>
+                  <td>All others</td>
+                  <td>-</td>
+                  <td>U</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+</div>
+</div>
