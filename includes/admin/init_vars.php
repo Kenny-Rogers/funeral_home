6b682200dca1e294063a2b4c13f403df12ae768c
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 04:03:26 +0000

    adding active class for links

--- includes/admin/init_vars.php
+++ includes/admin/init_vars.php
@@ -4,5 +4,12 @@ $full_name = "";    //used in public/admin/components/header.php
 $user = "";         //used in includes/admin/user_loader.php
 $page_loader = "";  //used in includes/admin/user_loader.php
 $menu = "";         //used in includes/admin/user_loader.php
-$display = "";      //used in includes/admin/page_loaders/*
+$display = "";      //used in includes/admin/loaders/*
+$page = "";         //used in includes/admin/loaders/*
+$class_ac = "";     //used in includes/admin/loaders/admin_loader.php
+$class_f = "";
+$class_r = "";
+$class_n = "";
+$class_as = "";
+$class_rs = "";
 ?>
