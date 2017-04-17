258c92541cb03b9de1bcee286aacc2eafb4aceb5
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 04:03:55 +0000

    defining more constants

--- includes/admin/initialize.php
+++ includes/admin/initialize.php
@@ -13,12 +13,23 @@ define('SITE_ROOT', DS.'var'.DS.'www'.DS.'html'.DS.'funeral_home');
 defined('LIB_PATH')?null:define('LIB_PATH', SITE_ROOT.DS.'includes');
 
 
+//MENU_COMPONENT::defines the path to the folder that contains the menus
+defined('MENU_COMPONENT')?null:
+define('MENU_COMPONENT', SITE_ROOT.DS."public".DS."admin".DS."components".DS."menu");
+
+//ADMIN_PAGES::defines the path to the folder that contains the pages
+//of the Administrator
+defined('ADMIN_PAGES')?null:
+define('ADMIN_PAGES', SITE_ROOT.DS."admin".DS."components".DS."pages".DS."admin");
+
+
 //load the config file first
 //require_once(LIB_PATH.DS."config.php");
 
 //load basic functions next so that the rest of the loaded files can
 //uses these two (config.php and functions.php)
 //require_once(LIB_PATH.DS.'functions.php');
+require_once(LIB_PATH.DS.'admin'.DS.'init_vars.php');
 require_once(LIB_PATH.DS.'admin'.DS.'user_loader.php');
 
 //load core objects
