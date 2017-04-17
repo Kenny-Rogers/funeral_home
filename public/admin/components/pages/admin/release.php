68f813870fbe57fa5dd4ac45bf0ea7cfda3b0117
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 08:28:14 +0000

    allows for a dead body to be released from the morgue

---
+++ public/admin/components/pages/admin/release.php
@@ -0,0 +1,41 @@
+<div class="row">
+        <div class="col-xs-12">
+          <div class="box box-primary">
+            <!--div class="box-header">
+              <h3 class="box-title"></h3>
+            </div-->
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Body Number</th>
+                  <th>Name</th>
+                  <th>Date of Death</th>
+                  <th>Relative</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                  <td>Misc</td>
+                  <td>PSP browser</td>
+                  <td>PSP</td>
+                  <td>-</td>
+                  <td><a href="?page=release_info&body_id=4">Release</a></td>
+                </tr>
+                <tr>
+                  <td>Other browsers</td>
+                  <td>All others</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>U</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+</div>
+</div>
