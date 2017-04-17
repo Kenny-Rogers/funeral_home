92883ef637a2aa3590debfa515a7cc733254a29e
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 14:11:10 +0000

    loads all requested pages of the attendant

---
+++ includes/admin/loaders/attendant_loader.php
@@ -0,0 +1,37 @@
+<?php
+//loads all the pages requested by the admin
+$page = isset($_GET['page']) ? $_GET['page'] : 'current_service';
+
+switch ($page) {
+  case 'current_service':
+    $display =  ATTENDANT_PAGES.DS."current_services.php";
+    $class_ac = "active";
+    break;
+
+  case 'find_info':
+    $display =  ADMIN_PAGES.DS."find_info.php";
+    $class_f = "active";
+    break;
+
+  case 'display_info':
+    $display =  ADMIN_PAGES.DS."display_info.php";
+    $class_f = "active";
+    break;
+
+  case 'release':
+    $display =  ATTENDANT_PAGES.DS."release.php";
+    $class_r = "active";
+    break;
+
+
+  case 'release_info':
+    $display =  ATTENDANT_PAGES.DS."release_info.php";
+    $class_r = "active";
+    break;
+
+  default:
+    $display =  ATTENDANT_PAGES.DS."current_services.php";
+    $class_ac = "active";
+    break;
+}
+?>
