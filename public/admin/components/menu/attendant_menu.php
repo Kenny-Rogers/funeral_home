970ec01b8e21b9a6e021a0fdc6fcf9c266847873
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 13:55:12 +0000

    menu options for morgue attendants

---
+++ public/admin/components/menu/attendant_menu.php
@@ -0,0 +1,16 @@
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu">
+      <li class="header">MORTURARY ATTENDANT</li>
+      <li class="<?php echo $class_ac; ?>"><a href="?page=current_service"><i class="fa fa-link"></i> <span>Services To Do</span></a></li>
+      <li class="<?php echo $class_f; ?>"><a href="?page=find_info"><i class="fa fa-link"></i> <span>Services For A Body</span></a></li>
+      <li class="<?php echo $class_r; ?>"><a href="?page=release"><i class="fa fa-link"></i> <span>Release Corpses</span></a></li>
+    </ul>
+    <!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
