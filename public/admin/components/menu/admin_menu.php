78f1cedfd84b4450d7b70f14c924c255c7be2564
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 04:06:13 +0000

    making link active on click of that link

---
+++ public/admin/components/menu/admin_menu.php
@@ -0,0 +1,21 @@
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu">
+      <li class="header">MORTURARY MANAGEMENT</li>
+      <li class="<?php echo $class_ac; ?>"><a href="?page=add_corpse"><i class="fa fa-link"></i> <span>Add Corpse</span></a></li>
+      <li class="<?php echo $class_f; ?>"><a href="?page=find_info"><i class="fa fa-link"></i> <span>Find Corpse Information</span></a></li>
+      <li class="<?php echo $class_r; ?>"><a href="?page=release"><i class="fa fa-link"></i> <span>Release Corpse</span></a></li>
+      <li class="<?php echo $class_n; ?>"><a href="?page=notify"><i class="fa fa-link"></i> <span>Notify Relative</span></a></li>
+
+      <li class="header">STAFF MANAGEMENT</li>
+      <li class="<?php echo $class_as; ?>"><a href="?page=add_staff"><i class="fa fa-link"></i> <span>Add Staff</span></a></li>
+      <li class="<?php echo $class_rs; ?>"><a href="?page=remove_staff"><i class="fa fa-link"></i> <span>Remove Staff</span></a></li>
+    </ul>
+    <!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
