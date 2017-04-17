20bb48bdb2091dd1080f453cbfb9bd44d533c1ac
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 14:40:54 +0000

    choose a body to record payment on

---
+++ public/admin/components/pages/accountant/add_payment.php
@@ -0,0 +1,41 @@
+<div class="row">
+        <div class="col-xs-12">
+          <div class="box box-primary">
+            <!--div class="box-header">
+              <h3 class="box-title"></h3>
+            </div-->
+            <!-- /.box-header -->
+            <div class="box-body">
+              <table id="example2" class="table table-bordered table-hover">
+                <thead>
+                <tr>
+                  <th>Body Number</th>
+                  <th>Name</th>
+                  <th>Date of Death</th>
+                  <th>Compartment Number</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                  <td>Misc</td>
+                  <td>PSP browser</td>
+                  <td>PSP</td>
+                  <td>-</td>
+                  <td><a href="?page=record_payment&body_id=4">Record Payment</a></td>
+                </tr>
+                <tr>
+                  <td>Other browsers</td>
+                  <td>All others</td>
+                  <td>-</td>
+                  <td>-</td>
+                  <td>U</td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+</div>
+</div>
