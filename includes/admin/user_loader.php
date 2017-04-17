0b739f8c2f813fd06eb1ed7584cfe97d59105343
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 02:46:55 +0000

    adding the page_loaders and menus for each user

--- includes/admin/user_loader.php
+++ includes/admin/user_loader.php
@@ -1,4 +1,26 @@
 <?php
 //loads all the content specific to a user{Administrator, Attendant, Accountant}
+$user = "";
 
+switch ($user) {
+  case 'admin':
+    $page_loader = LIB_PATH.DS."loaders".DS."admin_loader.php";
+    $menu = SITE_ROOT.DS."public".DS."components".DS."menu"."admin_menu.php";
+    break;
+
+  case 'attendant':
+    $page_loader = LIB_PATH.DS."loaders".DS."attendant_loader.php";
+    $menu = SITE_ROOT.DS."public".DS."components".DS."menu"."admin_menu.php";
+    break;
+
+  case 'accountant':
+    $page_loader = LIB_PATH.DS."loaders".DS."accountant_loader.php";
+    $menu = SITE_ROOT.DS."public".DS."components".DS."menu"."admin_menu.php";
+    break;
+
+  default:
+    $page_loader = LIB_PATH.DS."loaders".DS."attendant_loader.php";
+    $menu = SITE_ROOT.DS."public".DS."components".DS."menu"."admin_menu.php";
+    break;
+}
 ?>
