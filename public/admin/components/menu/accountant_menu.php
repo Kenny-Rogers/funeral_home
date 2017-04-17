acf9be77ef7d7df3316263604002d602d41a49ae
Kenny-Rogers <ylodonu@gmail.com>
2017-04-17 14:17:46 +0000

    menu to load accountant options

---
+++ public/admin/components/menu/accountant_menu.php
@@ -0,0 +1,15 @@
+<aside class="main-sidebar">
+
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+
+    <!-- Sidebar Menu -->
+    <ul class="sidebar-menu">
+      <li class="header">MORTURARY ACCOUNTANT</li>
+      <li class="<?php echo $class_ac; ?>"><a href="?page=add_payment"><i class="fa fa-link"></i> <span>Add Payment</span></a></li>
+      <li class="<?php echo $class_f; ?>"><a href="?page=find_payment"><i class="fa fa-link"></i> <span>Find Payment</span></a></li>
+    </ul>
+    <!-- /.sidebar-menu -->
+  </section>
+  <!-- /.sidebar -->
+</aside>
