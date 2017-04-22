19464e5f71ba6a1b937a89e3d6f269e7415db655
Kenny-Rogers <ylodonu@gmail.com>
2017-04-22 04:54:44 +0000

    database confiigurations

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+//db constants
+defined('DB_SERVER')? null : define("DB_SERVER", "localhost");
+defined('DB_USER')? null : define("DB_USER", "webapp");
+defined('DB_PASS')? null : define("DB_PASS", "funeral_home");
+defined('DB_NAME')? null : define("DB_NAME", "funeral_home");
+?>
