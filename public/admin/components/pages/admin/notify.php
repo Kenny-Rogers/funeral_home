cadbdf7ee63ee77e78593a9100013be427a012a9
Kenny-Rogers <ylodonu@gmail.com>
2017-04-23 21:42:37 +0000

    adding process status features

--- public/admin/components/pages/admin/notify.php
+++ public/admin/components/pages/admin/notify.php
@@ -1,3 +1,30 @@
+<?php
+  $deadbodies = Deadbody::find_all(" WHERE NOT status='released'");
+
+  $status = isset($_GET['stat'])?$_GET['stat']:0;
+
+  switch ($status) {
+    case 1:
+      $message = "Notification successfull";
+      $class = "success";
+      break;
+
+    case 2:
+      $message = "Notification failed";
+      $class = "fail";
+      break;
+
+    default:
+      $message = "Please Complete Form";
+      $class = "info";
+      break;
+  }
+?>
+<div class="row">
+  <div class="col-md-offset-2 col-md-10">
+    <?php output_message($message, $class)?>
+  </div>
+</div>
 <div class="row">
 <!-- left column -->
 <div class="col-md-offset-2 col-md-8">
@@ -13,36 +40,25 @@
       <div class="form-group">
         <label for="">Body Name</label>
         <select class="form-control" name="body_id">
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
+          <?php foreach ($deadbodies as $deadbody) { ?>
+            <option value="<?php echo $deadbody->id; ?>"><?php echo $deadbody->get_full_name(); ?></option>
+          <?php } ?>
         </select>
       </div>
       <div class="form-group">
         <label for="">Choose Template</label>
-        <select class="form-control" name="Religion">
-          <option value="-1">Choose ........</option>
+        <select class="form-control" name="message">
+          <!--option value="-1">Choose ........</option-->
           <option value="1">Notice of debt in relation to corpse</option>
           <option value="2">Release of dead body</option>
           <option value="-2">Others..</option>
         </select>
       </div>
-      <!--div class="form-group">
-        <label for="">Date of Birth</label>
-        <input type="date" class="form-control"  placeholder="dd/mm/yyyy" name="DateOfBirth">
-      </div>
-      <div class="form-group">
-        <label for="">Date of Death</label>
-        <input type="date" class="form-control"  placeholder="dd/mm/yyyy" name="DateOfBirth">
-      </div-->
       <div class="form-group">
         <label for="">*(If other is chosen in previous step then specify)</label>
         <textarea class="form-control"  placeholder="Password" name="other">
         </textarea>
       </div>
-      <!--div class="form-group">
-        <label for="">Cause Of Death</label>
-        <input type="text" class="form-control"  placeholder="Short description of cause of death" name="CauseOfDeath">
-      </div-->
     </div>
     <!-- /.box-body -->
 
