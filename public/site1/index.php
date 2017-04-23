3464f15c816f7a2219c36e9913ef2a3aaa737419
Kenny-Rogers <ylodonu@gmail.com>
2017-04-23 14:00:41 +0000

    including the dynamic content

--- public/site1/index.php
+++ public/site1/index.php
@@ -1,9 +1,11 @@
+<?php include("../../includes/admin/initialize.php");?>
+
 <?php include("../../includes/public/page_loader.php"); ?>
 
 <?php include("components/header.php"); ?>
 
 <?php include("components/nav_bar.php"); ?>
 
-<?php include("components/main_content.php"); ?>
+<?php include("{$display}"); ?>
 
 <?php include("components/footer.php");?>
