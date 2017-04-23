1846193fce62c8cf99c1ed9cc114b6dfa4d148f3
Kenny-Rogers <ylodonu@gmail.com>
2017-04-23 13:41:09 +0000

    retreiving the info of logged in user

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,5 +1,14 @@
 <?php include("../../includes/admin/initialize.php");?>
 
+<?php if(!$session->is_logged_in()){
+          redirect_to('login.php');
+      }else{
+          $worker_id = $session->worker_id;
+          $worker = Worker::find_by_id($worker_id);
+      } ?>
+
+<?php include("../../includes/admin/user_loader.php");?>
+
 <?php include("components/head.php"); ?>
 
 <?php include("components/header.php"); ?>
