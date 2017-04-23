30b31f8b118f173cf26c1513a2065fb6906d4355
Kenny-Rogers <ylodonu@gmail.com>
2017-04-23 15:13:10 +0000

    assigning info to an object and saving the object

--- includes/admin/action/register_body.php
+++ includes/admin/action/register_body.php
@@ -1,3 +1,28 @@
 <?php
-echo "...";
+  require_once("../initialize.php");
+  //register deadbody
+
+  $deadbody = new Deadbody();
+  if(isset($_POST)){
+    $deadbody->set_full_name($_POST["full_name"]);
+    $deadbody->set_gender($_POST["gender"]);
+    $deadbody->set_address($_POST["address"]);
+    $deadbody->set_religion($_POST["religion"]);
+    $date=format_date(strtotime($_POST["date_of_birth"]));
+    $deadbody->set_date_of_birth($date);
+    $date=format_date(strtotime($_POST["date_of_death"]));
+    $deadbody->set_date_of_death($date);
+    $deadbody->set_cause_of_death($_POST["cause_of_death"]);
+    $deadbody->set_status("admitted");
+
+    if ($deadbody && $deadbody->save()){
+        $worker_id = $session->worker_id;
+        $worker = Worker::find_by_id($worker_id);
+        $worker_name = $worker->full_name();
+        Log::log_action("Registration", "{$full_name} registered Deadbody ".$deadbody->get_full_name());
+        redirect_to("../../../public/admin/index.php?page=add_corpse&stat=1");
+    }else{
+        redirect_to("../../../public/admin/index.php?page=add_corpse&stat=2");
+    }
+  }
 ?>
