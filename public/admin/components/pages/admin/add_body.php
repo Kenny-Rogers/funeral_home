80424cab893132fb99be47ed2d1b33d49eb52daf
Kenny-Rogers <ylodonu@gmail.com>
2017-04-23 15:12:02 +0000

    adding registration status

--- public/admin/components/pages/admin/add_body.php
+++ public/admin/components/pages/admin/add_body.php
@@ -1,3 +1,28 @@
+<?php
+ $status = isset($_GET['stat'])?$_GET['stat']:0;
+
+ switch ($status) {
+   case 1:
+     $message = "Registration successfull";
+     $class = "success";
+     break;
+
+   case 2:
+     $message = "Registration failed";
+     $class = "fail";
+     break;
+
+   default:
+     $message = "Please Complete Form";
+     $class = "info";
+     break;
+ }
+?>
+<div class="row">
+  <div class="col-md-offset-2 col-md-10">
+    <?php output_message($message, $class)?>
+  </div>
+</div>
 <div class="row">
 <!-- left column -->
 <div class="col-md-offset-2 col-md-8">
@@ -12,18 +37,18 @@
     <div class="box-body">
       <div class="form-group">
         <label for="">Full Name</label>
-        <input type="text" class="form-control"  placeholder="Enter Full Name" name="FullName">
+        <input type="text" class="form-control"  placeholder="Enter Full Name" name="full_name" required>
       </div>
       <div class="form-group">
         <label for="">Gender</label>
-        <select class="form-control" name="Gender">
+        <select class="form-control" name="gender" required>
           <option value="Male">Male</option>
           <option value="Female">Female</option>
         </select>
       </div>
       <div class="form-group">
         <label for="">Religion</label>
-        <select class="form-control" name="Religion">
+        <select class="form-control" name="religion" required>
           <option value="Christian">Christian</option>
           <option value="Muslim">Muslim</option>
           <option value="Others">Others..</option>
@@ -31,26 +56,28 @@
       </div>
       <div class="form-group">
         <label for="">Date of Birth</label>
-        <input type="date" class="form-control"  placeholder="dd/mm/yyyy" name="DateOfBirth">
+        <input type="date" class="form-control"  placeholder="dd/mm/yyyy" name="date_of_birth" required>
       </div>
       <div class="form-group">
         <label for="">Date of Death</label>
-        <input type="date" class="form-control"  placeholder="dd/mm/yyyy" name="DateOfBirth">
+        <input type="date" class="form-control"  placeholder="dd/mm/yyyy" name="date_of_death" required>
       </div>
       <div class="form-group">
         <label for="">Address</label>
-        <textarea class="form-control"  placeholder="Password" name="Address">
+        <textarea class="form-control"  placeholder="Password" name="address" required >
         </textarea>
       </div>
       <div class="form-group">
         <label for="">Cause Of Death</label>
-        <input type="text" class="form-control"  placeholder="Short description of cause of death" name="CauseOfDeath">
+        <input type="text" class="form-control"  placeholder="Short description of cause of death"
+              name="cause_of_death" required>
       </div>
     </div>
     <!-- /.box-body -->
 
     <div class="box-footer">
       <button type="submit" class="btn btn-primary">Register</button>
+      <button type="reset" class="btn btn-info">Reset Form</button>
     </div>
   </form>
 </div>
