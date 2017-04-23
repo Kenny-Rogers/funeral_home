a117b07c302752b0dd916d24af12bcb909266fec
Kenny-Rogers <ylodonu@gmail.com>
2017-04-23 10:18:16 +0000

    changing access modifiers

--- includes/models/worker.php
+++ includes/models/worker.php
@@ -2,26 +2,26 @@
 require_once(LIB_PATH.DS.'database.php');
 
 class Worker extends DatabaseObject {
-  //this class interacts with the user table in the database
+  //this class interacts with the worker table in the database
   //NOTE::for very large tables, you can use mysqli's show_fields_from(TABLE_NAME)
   //get a list of attributes to be used
   protected static $table_name="worker";
   protected static $db_fields=array('id', 'email', 'password',
     'full_name', 'gender', 'role', 'telephone');
   public $id;
-  public $full_name;
-  public $password;
-  public $gender;
-  public $role;
-  public $email;
-  public $telephone;
+  protected $full_name;
+  protected $password;
+  protected $gender;
+  protected $role;
+  protected $email;
+  protected $telephone;
 
   public static function authenticate($email="", $password=""){
     //validates email and password
     global $database;
     $email=$database->escape_value($email);
     $password=$database->escape_value($password);
-    $sql="SELECT * FROM users WHERE email='{$email}'
+    $sql="SELECT * FROM {$table_name} WHERE email='{$email}'
           AND password='{$password}' LIMIT 1";
     $result_array=self::find_by_sql($sql);
     //returns the first element in the array
