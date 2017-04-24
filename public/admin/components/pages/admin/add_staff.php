2b7f009a320333dd99336abb0710f08829e24c7c
Kenny-Rogers <ylodonu@gmail.com>
2017-04-24 04:09:44 +0000

    adding a new staff

--- public/admin/components/pages/admin/add_staff.php
+++ public/admin/components/pages/admin/add_staff.php
@@ -1,3 +1,28 @@
+<?php
+ $status = isset($_GET['stat'])?$_GET['stat']:0;
+
+ switch ($status) {
+   case 1:
+     $message = "Registration successfull";
+     $class = "success";
+     break;
+
+   case 2:
+     $message = "Registration failed";
+     $class = "fail";
+     break;
+
+   default:
+     $message = "Please Complete Form";
+     $class = "info";
+     break;
+ }
+?>
+<div class="row">
+  <div class="col-md-offset-2 col-md-10">
+    <?php output_message($message, $class)?>
+  </div>
+</div>
 <div class="row">
 <!-- left column -->
 <div class="col-md-offset-2 col-md-8">
@@ -12,30 +37,30 @@
     <div class="box-body">
       <div class="form-group">
         <label for="">Full Name</label>
-        <input type="text" class="form-control"  placeholder="Enter Full Name" name="FullName">
+        <input type="text" class="form-control"  placeholder="Enter Full Name" name="full_name">
       </div>
       <div class="form-group">
         <label for="">Role</label>
-        <select class="form-control" name="Role">
-          <option value="Attendant">Attendant</option>
-          <option value="Accountant">Accountant</option>
-          <option value="Administrator">Administrator</option>
+        <select class="form-control" name="role">
+          <option value="attendant">Attendant</option>
+          <option value="accountant">Accountant</option>
+          <option value="admin">Administrator</option>
         </select>
       </div>
       <div class="form-group">
         <label for="">Gender</label>
-        <select class="form-control" name="Gender">
+        <select class="form-control" name="gender">
           <option value="Male">Male</option>
           <option value="Female">Female</option>
         </select>
       </div>
       <div class="form-group">
         <label for="">Email</label>
-        <input type="text" class="form-control"  placeholder="Enter Email" name="Email">
+        <input type="text" class="form-control"  placeholder="Enter Email" name="email">
       </div>
       <div class="form-group">
         <label for="">Telephone</label>
-        <input type="telephone" class="form-control"  placeholder="Telephone" name="Telephone">
+        <input type="telephone" class="form-control"  placeholder="Telephone" name="telephone">
       </div>
       <!-- /.box-body -->
 
