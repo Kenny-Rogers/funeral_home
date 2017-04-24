79c476cc620e30c445cb6a0e4facc033ea3c5370
Kenny-Rogers <ylodonu@gmail.com>
2017-04-24 04:11:40 +0000

    definig attributes

--- includes/models/requested_service.php
+++ includes/models/requested_service.php
@@ -8,9 +8,10 @@ class RequestedService extends DatabaseObject {
   protected static $table_name="requested_service";
   protected static $db_fields=array('rel_no', 'dead_no',
     'service_no', 'worker_no');
-  protected $rel_no;
-  protected $dead_no;
-  protected $service_no;
-  protected $worker_no;
+  public $id;
+  public $rel_no;
+  public $dead_no;
+  public $service_no;
+  public $worker_no;
 }
 ?>
