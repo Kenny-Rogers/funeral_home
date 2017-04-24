bbf3133728049fa23fcf860c5806b12e7f6aeaea
Kenny-Rogers <ylodonu@gmail.com>
2017-04-24 09:37:29 +0000

    display services of the company

---
+++ public/site1/pages/service.php
@@ -0,0 +1,90 @@
+<div id="contentOuterSeparator"></div>
+
+<div class="container">
+
+    <div class="divPanel page-content">
+
+        <div class="breadcrumbs">
+                <a href="index.html">Home</a> &nbsp;/&nbsp; <span>Simple</span>
+        </div>
+
+        <div class="row-fluid">
+			<!--Edit Main Content Area here-->
+                <div class="span8" id="divMain">
+
+                    <font color ="violet" size ="8"> Services overview</font><br></br>
+                    <font face ="Agency FB" size ="5"> Life is full of of opportunities to show someone we love them. One such opportunity is the funeral or memorial services. Those events
+                      are meant to celebrate the choices they made, the relationships you shared and honors the memory of your loved one.
+                      Here are some of the services we provide at CrossOver mortuary and funeral home: </font><br></br>
+			<div class="row-fluid">
+		        <div class="span2">
+                    <img src="images/services-image-3.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
+
+                <div class="span10">
+                    <font face ="Agency FB" size ="5"> <font color ="violet">Body storage</font><br></br>Lorem ipsum dolor sit amet, consectetueradipiscing elied diam nonummy nibh euisod tincidunt ut laoreet dolore magna aliquam erat volutpatorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                                          sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</font>
+                </div>
+      </div>
+
+			<hr>
+
+      <div class="row-fluid">
+		        <div class="span2">
+                    <img src="images/services-image-3.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
+
+                <div class="span10">
+                  <font face ="Agency FB" size ="5"> <font color ="violet">Body Treatments(embalment)</font><br></br>Lorem ipsum dolor sit amet, consectetueradipiscing elied diam nonummy nibh euisod tincidunt
+                                                                          ut laoreet dolore magna aliquam erat volutpatorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                                                          sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                                                                          quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</font>
+                </div>
+      </div>
+
+			<hr>
+      <div class="row-fluid">
+		        <div class="span2">
+                    <img src="images/services-image-3.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
+
+                <div class="span10">
+                    <font face ="Agency FB" size ="5"> <font color ="violet">Casketting</font><br></br>Lorem ipsum dolor sit amet, consectetueradipiscing elied diam nonummy nibh euisod tincidunt
+                                                                            ut laoreet dolore magna aliquam erat volutpatorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                                                            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                                                                            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</font>
+                </div>
+      </div>
+
+			<hr>
+      <div class="row-fluid">
+		        <div class="span2">
+                    <img src="images/services-image-3.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
+                    <div class="span10">
+                              <font face ="Agency FB" size ="5"> <font color ="violet">Dressing(cossetting)</font><br></br>Lorem ipsum dolor sit amet, consectetueradipiscing elied diam nonummy nibh euisod tincidunt
+                                                                                      ut laoreet dolore magna aliquam erat volutpatorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                                                                      sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                                                                                      quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</font>
+                  </div>
+      </div>
+
+			<hr>
+      <div class="row-fluid">
+		        <div class="span2">
+                    <img src="images/services-image-3.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
+
+                    <div class="span10">
+                    <font face ="Agency FB" size ="5"> <font color ="violet">Cemetary and Pallbearer seletion</font><br></br>Lorem ipsum dolor sit amet, consectetueradipiscing elied diam nonummy nibh euisod tincidunt
+                                                                            ut laoreet dolore magna aliquam erat volutpatorem ipsum dolor sit amet, consectetuer adipiscing elit,
+                                                                            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+                                                                            quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</font>
+                </div>
+                </div>
+      </div>
+
+
+                </div>
+
+        </div>
+
+        <div id="footerInnerSeparator"></div>
+    </div>
+
+</div>
