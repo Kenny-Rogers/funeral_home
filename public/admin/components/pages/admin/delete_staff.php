eb1dc46f66992c76c7daf9ee9014bc44cc6031ee
Kenny-Rogers <ylodonu@gmail.com>
2017-04-24 04:09:29 +0000

    page to delete staff

--- public/admin/components/pages/admin/delete_staff.php
+++ public/admin/components/pages/admin/delete_staff.php
@@ -1,3 +1,23 @@
 <?php
-echo "q";
+  $staff_id = $_GET["worker_id"];
+  $staff = Worker::find_by_id($staff_id);
+  $staff->set_status("inactive");
+
+  if ($staff && $staff->save()){
+      $worker = Worker::find_by_id($session->worker_id);
+      $worker_name = $worker->get_full_name();
+      $staff_name = $staff->get_full_name();
+      Log::log_action("Released", "{$worker_name} released staff {$staff_name}");
+      $message = "Staff released successfully";
+      $class = "success";
+  }else{
+    $message = "Failed to release staff";
+    $class = "fail";
+  }
 ?>
+
+<div class="row">
+  <div class="col-md-offset-2 col-md-10">
+    <?php output_message($message, $class)?>
+  </div>
+</div>
