e51bf8aa101bbd7897461f5e8ae3cf9474d5a5cb
Kenny-Rogers <ylodonu@gmail.com>
2017-04-24 09:42:30 +0000

    adding content

--- public/site1/pages/about_us.php
+++ public/site1/pages/about_us.php
@@ -10,36 +10,36 @@
 			<!--Edit Main Content Area here-->
                 <div class="span8" id="divMain">
 
-                    <h1>About Us</h1>
+                    <font color ="violet" size ="8"> About Us</font>
 					<hr>
-                    <h3>Mission</h3>
-                    <p>The mission of CrossOver mortuary and funeral home is to take the best care of families and friends in the time of a lost loved one by providing homely
+                    <font color ="violet" size = "5">Mission</font><br></br>
+                    <font face ="Agency FB" size ="5">The mission of CrossOver mortuary and funeral home is to take the best care of families and friends in the time of a lost loved one by providing homely
                       environment.
-                      </p>
+                    </font>
                     <hr>
 
 
-					<h3>Vision</h3>
+					<font color ="violet" size = "5">Vision</font>
 
 			<div class="row-fluid">
 		        <div class="span2">
                     <img src="images/services-image-5.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
                 <div class="span10">
-                    <p>The vision of the mortuary and funeral home is to be sucesful with the best services.
+                    <font face ="Agency FB" size ="5">The vision of the mortuary and funeral home is to be sucesful with the best services.
                     This will be attained through provision of the highest quality services with great facilities, well trained and experienced personnel
                     at the most affordable rates. <br/> <br/>
                     In order to achieve the set mission and vision, the funeral home will develop a culture of combination of healthy work environment,
-                  hardwork, and family and customer service. </p>
+                  hardwork, and family and customer service. </font>
                 </div>
             </div>
 
 			<hr>
-         <h3>Our Goals</h3>
+         <font color ="violet" size = "5">Our Goals</font>
 			<div class="row-fluid">
 		        <div class="span2">
                     <img src="images/services-image-3.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
                 <div class="span10">
-                    <ul>
+                    <ul style="font-family:Agency FB font:size=65 ">
                       <li>To be open to new ideas and treat them as challenges.</li>
                      <li>To remember the small details among the important larger ones.</li>
                      <li>To put ourselves in the place of others and ask "Have we done enough?".</li>
@@ -50,69 +50,10 @@
                     </ul>
                 </div>
             </div>
-
 			<hr>
 
- <!--<h3>Contact Us</h3>
-			<div class="row-fluid">
-		        <div class="span2">
-                    <img src="images/services-image-4.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">   </div>
-                <div class="span10">
-                    <p>Get in touch</br>
-                      We are always standing by and eager to help.
-                    </p>
-                </div>
-            </div>-->
-
           </div>
-				<!--End Main Content Area here-->
 
-				<!--Edit Sidebar Content here-->
-                <!--<div class="span4 sidebar">
-
-                    <div class="sidebox">
-                        <h3 class="sidebox-title">Sample Sidebar Content</h3>
-                        <p>Lorem Ipsum is simply dummy text of the printing and <a href="#">typesetting industry</a>.</p>
-						<p> Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s.</p>
-
-                        <h4 class="sidebox-title">Sidebar Categories</h4>
-                          <ul>
-                            <li><a href="#">Quisque diam lorem sectetuer adipiscing</a></li>
-                            <li><a href="#">Interdum vitae, adipiscing dapibus ac</a></li>
-                            <li><a href="#">Scelerisque ipsum auctor vitae, pede</a></li>
-                            <li><a href="#">Donec eget iaculis lacinia non erat</a></li>
-                            <li><a href="#">Lacinia dictum elementum velit fermentum</a></li>
-                            <li><a href="#">Donec in velit vel ipsum auctor pulvinar</a></li>
-                          </ul>
-
-
-				       	  <h4 class="sidebox-title">Our Skills</h4>
-
-				            	<h5>Web Design ( 40% )</h5>
-				            	<div class="progress progress-info">
-                                <div class="bar" style="width: 20%"></div>
-                                </div>
-
-				            	<h5>Wordpress ( 80% )</h5>
-				            	<div class="progress progress-success">
-                                <div class="bar" style="width: 40%"></div>
-                                </div>
-
-				            	<h5>Branding ( 100% )</h5>
-				            	<div class="progress progress-warning">
-                                <div class="bar" style="width: 60%"></div>
-                                </div>
-
-				            	<h5>SEO Optmization ( 60% )</h5>
-				            	<div class="progress progress-danger">
-                                <div class="bar" style="width: 80%"></div>
-                                </div>
-
-
-                    </div>
-
-                </div>-->
-				<!--End Sidebar Content here-->
             </div>
 
         <div id="footerInnerSeparator"></div>
