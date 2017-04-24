1351da68809f467ab076a863106852ae62a62ffc
Kenny-Rogers <ylodonu@gmail.com>
2017-04-24 04:13:53 +0000

    save new staff details to db

--- includes/admin/action/register_staff.php
+++ includes/admin/action/register_staff.php
@@ -1,3 +1,25 @@
 <?php
-echo "...";
+  require_once("../initialize.php");
+  //register staff
+
+  $staff = new Worker();
+  if(isset($_POST)){
+    $staff->set_full_name($_POST["full_name"]);
+    $staff->set_gender($_POST["gender"]);
+    $staff->set_role($_POST["role"]);
+    $staff->set_email($_POST["email"]);
+    $staff->set_telephone($_POST["telephone"]);
+    $staff->set_password();
+    $staff->set_status();
+
+    if ($staff && $staff->save()){
+        $worker = Worker::find_by_id($session->worker_id);
+        $worker_name = $worker->get_full_name();
+        $staff_name = $staff->get_full_name();
+        Log::log_action("Registration", "{$worker_name} registered staff {$staff_name}");
+        redirect_to("../../../public/admin/index.php?page=add_staff&stat=1");
+    }else{
+        redirect_to("../../../public/admin/index.php?page=add_staff&stat=2");
+    }
+  }
 ?>
