6ca9c6a9e17bc67aa38b872c6cd20e2f6e28ec6d
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 10:44:52 +0000

    including debit and credit in table

--- public/admin/components/pages/admin/release.php
+++ public/admin/components/pages/admin/release.php
@@ -12,9 +12,10 @@
                 <tr>
                   <th>Body Number</th>
                   <th>Name</th>
-                  <th>Date of Death</th>
-                  <th>Days spent</th>
+                  <th>Days Spent</th>
                   <th>Relative</th>
+                  <th>Total Debit(GH &#8373;)</th>
+                  <th>Total Credit(GH &#8373;)</th>
                   <th>Action</th>
                 </tr>
                 </thead>
@@ -23,9 +24,10 @@
                     <tr>
                     <td><?php echo $deadbody->id; ?></td>
                     <td><?php echo $deadbody->get_full_name(); ?></td>
-                    <td><?php echo $deadbody->get_date_of_death(); ?></td>
                     <td><?php echo $deadbody->get_days_spent();?></td>
                     <td>-</td>
+                    <td><?php echo $deadbody->get_total_debit();?></td>
+                    <td><?php echo $deadbody->get_total_credit();?></td>
                     <td><a href="?page=release_info&body_id=<?php echo $deadbody->id; ?>">Release</a></td>
                   </tr>
                   <?php endforeach; ?>
