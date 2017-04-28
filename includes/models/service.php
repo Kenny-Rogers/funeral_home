b0517398670425900a058b25eb7224f1295cbcb3
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 10:45:42 +0000

    adding new field

--- includes/models/service.php
+++ includes/models/service.php
@@ -4,10 +4,11 @@ require_once(LIB_PATH.DS.'database.php');
 class Service extends DatabaseObject {
   //this class interacts with the service table in the database
   protected static $table_name="service";
-  protected static $db_fields=array('id', 'name', 'description');
+  protected static $db_fields=array('id', 'name', 'description', 'price');
   public $id;
   public $name;
   public $description;
+  public $price;
 
 }
 ?>
