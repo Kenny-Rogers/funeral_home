329b2826a4ac7e9ba215827968a5d2a752fd2da1
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 00:33:49 +0000

    making attributes public adding comments

--- includes/models/payment.php
+++ includes/models/payment.php
@@ -3,17 +3,16 @@ require_once(LIB_PATH.DS.'database.php');
 
 class Payment extends DatabaseObject {
   //this class interacts with the payment table in the database
-  //NOTE::for very large tables, you can use mysqli's show_fields_from(TABLE_NAME)
-  //get a list of attributes to be used
   protected static $table_name="payment";
   protected static $db_fields=array('id', 'amount', 'paid_by',
     'paid_for', 'date_paid', 'description');
   public $id;
-  protected $amount;
-  protected $paid_by;
-  protected $paid_for;
-  protected $date_paid;
-  protected $description;
+  public $amount;
+  public $paid_by;
+  public $paid_for;
+  public $date_paid;
+  public $description;
+
 
 }
 ?>
