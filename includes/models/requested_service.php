055496d823de3ad2fb268d7da1ec5f42170e1cfb
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 00:33:11 +0000

    adding comments

--- includes/models/requested_service.php
+++ includes/models/requested_service.php
@@ -2,9 +2,7 @@
 require_once(LIB_PATH.DS.'database.php');
 
 class RequestedService extends DatabaseObject {
-  //this class interacts with the payment table in the database
-  //NOTE::for very large tables, you can use mysqli's show_fields_from(TABLE_NAME)
-  //get a list of attributes to be used
+  //this class interacts with the relative table in the database
   protected static $table_name="requested_service";
   protected static $db_fields=array('rel_no', 'dead_no',
     'service_no', 'worker_no');
