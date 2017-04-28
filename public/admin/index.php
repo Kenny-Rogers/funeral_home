3d45e1d7038ad7cbfdf6059becd13267cef6a62b
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 00:29:22 +0000

    including code to check if user is logged in

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,11 +1,13 @@
 <?php include("../../includes/admin/initialize.php");?>
 
-<?php if(!$session->is_logged_in()){
+<?php
+      if(!$session->is_logged_in()){
           redirect_to('login.php');
       }else{
           $worker_id = $session->worker_id;
           $worker = Worker::find_by_id($worker_id);
-      } ?>
+    }
+?>
 
 <?php include("../../includes/admin/user_loader.php");?>
 
