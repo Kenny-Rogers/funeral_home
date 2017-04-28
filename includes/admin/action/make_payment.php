c78bb73cb06cc4ae162fd1dd2552d42d5d47740a
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 10:45:20 +0000

    changing log sentence

--- includes/admin/action/make_payment.php
+++ includes/admin/action/make_payment.php
@@ -4,12 +4,10 @@ require_once("../initialize.php");
 
 //creates payment object
 $payment = new Payment();
-$date = strftime('%Y-%m-%d %H:%M:%S', time());
 if(isset($_POST)){
   $payment->amount = $_POST["amount"];
   $payment->paid_by = $_POST["paid_by"];
   $payment->paid_for = $_POST["paid_for"];
-  $payment->date_paid = $date;
   $payment->description = $_POST["description"];
 
   if ($payment && $payment->save()){
@@ -17,7 +15,7 @@ if(isset($_POST)){
       $worker = Worker::find_by_id($session->worker_id);
       $worker_name = $worker->get_full_name();
       $name = Deadbody::find_by_id($payment->paid_for)->get_full_name();
-      Log::log_action("Accounts", "{$worker_name} recorded payment {$name}");
+      Log::log_action("Accounts", "{$worker_name} recorded payment for {$name}");
       redirect_to("../../../public/admin/index.php?page=add_payment&stat=1");
   }else{
       redirect_to("../../../public/admin/index.php?page=add_payment&stat=2");
