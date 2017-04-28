b5f044587abe48c51da1da1185bb739b88bdc9de
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 02:37:04 +0000

    displays information on payment for deadbody

---
+++ public/admin/components/pages/general/payment_info.php
@@ -0,0 +1,12 @@
+<div class="panel panel-primary">
+      <div class="panel-heading">
+        <h4 class="panel-title">
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">Payment Information</a>
+        </h4>
+      </div>
+      <div id="collapse4" class="panel-collapse collapse">
+        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+      </div>
+</div>
