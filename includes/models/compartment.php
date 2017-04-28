9a0af0a11ebdd65730da430483680078a10f3286
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 00:34:21 +0000

    adding comments

--- includes/models/compartment.php
+++ includes/models/compartment.php
@@ -2,14 +2,12 @@
 require_once(LIB_PATH.DS.'database.php');
 
 class Compartment extends DatabaseObject {
-  //this class interacts with the payment table in the database
-  //NOTE::for very large tables, you can use mysqli's show_fields_from(TABLE_NAME)
-  //get a list of attributes to be used
+  //this class interacts with the compartment table in the database
   protected static $table_name="compartment";
   protected static $db_fields=array('id', 'description', 'suitable_for');
   public $id;
-  protected $suitable_for;
-  protected $description;
+  public $suitable_for;
+  public $description;
 
 }
 ?>
