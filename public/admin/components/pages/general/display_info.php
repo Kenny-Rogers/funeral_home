a0f3654cabac2675675242cd0ea4c994f27dbba7
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 03:04:50 +0000

    handling case where body_id doesnot exist

--- public/admin/components/pages/general/display_info.php
+++ public/admin/components/pages/general/display_info.php
@@ -1,15 +1,26 @@
 <?php
   $body_id=$_GET["body_id"];
   $deadbody = Deadbody::find_by_id($body_id);
-  $deadbody->compartment = array_shift(Compartment::find_all(" WHERE dead_no='$body_id' LIMIT 1"));
-  $services = RequestedService ::find_by_sql("SELECT * FROM requested_service WHERE dead_no='{$body_id}'");
-
+  if ($deadbody) {
+    $deadbody->compartment = array_shift(Compartment::find_all(" WHERE dead_no='$body_id' LIMIT 1"));
+    $services = RequestedService ::find_by_sql("SELECT * FROM requested_service WHERE dead_no='{$body_id}'");
 ?>
-<!-- including page to display body information -->
-<?php include(GENERAL_PAGES.DS."body_info.php");?>
+    <!-- including page to display body information -->
+    <?php include(GENERAL_PAGES.DS."body_info.php");?>
 
-<!-- including page to display service information -->
-<?php include(GENERAL_PAGES.DS."service_info.php"); ?>
+    <!-- including page to display service information -->
+    <?php include(GENERAL_PAGES.DS."service_info.php"); ?>
 
-<!-- including page to display payment information -->
-<?php include(GENERAL_PAGES.DS."payment_info.php")?>
+    <!-- including page to display payment information -->
+    <?php include(GENERAL_PAGES.DS."payment_info.php"); ?>
+<?php
+  } else {
+      $message = "No record found";
+      $class = "fail";
+?>
+    <div class="row">
+      <div class="col-md-offset-2 col-md-10">
+        <?php output_message($message, $class)?>
+      </div>
+    </div>
+<?php } ?>
