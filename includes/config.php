df06d6cb0124db6621de1f3a8707c0c1979152de
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 00:31:44 +0000

    adding comments

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,5 @@
 <?php
-
-//db constants
+//database connection constants
 defined('DB_SERVER')? null : define("DB_SERVER", "localhost");
 defined('DB_USER')? null : define("DB_USER", "webapp");
 defined('DB_PASS')? null : define("DB_PASS", "funeral_home");
