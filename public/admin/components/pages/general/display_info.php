2a288a43d38300c663e1b90886cb689830a5c064
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 03:19:48 +0000

    allowing for display from successfull body registration

--- public/admin/components/pages/general/display_info.php
+++ public/admin/components/pages/general/display_info.php
@@ -1,10 +1,22 @@
 <?php
   $body_id=$_GET["body_id"];
   $deadbody = Deadbody::find_by_id($body_id);
+
   if ($deadbody) {
+    //if the body_id provides a body
     $deadbody->compartment = array_shift(Compartment::find_all(" WHERE dead_no='$body_id' LIMIT 1"));
     $services = RequestedService ::find_by_sql("SELECT * FROM requested_service WHERE dead_no='{$body_id}'");
 ?>
+    <?php
+        if (isset($_GET["stat"])) {
+        //if user came to page from registration page
+    ?>
+      <div class="row">
+        <div class="col-md-offset-2 col-md-10">
+          <?php output_message("Body Registration Successfull", "success"); ?>
+        </div>
+      </div>
+    <?php } ?>
     <!-- including page to display body information -->
     <?php include(GENERAL_PAGES.DS."body_info.php");?>
 
@@ -13,14 +25,12 @@
 
     <!-- including page to display payment information -->
     <?php include(GENERAL_PAGES.DS."payment_info.php"); ?>
-<?php
-  } else {
-      $message = "No record found";
-      $class = "fail";
+<?php } else {
+      //if the body_id doesnot exist
 ?>
     <div class="row">
       <div class="col-md-offset-2 col-md-10">
-        <?php output_message($message, $class)?>
+        <?php output_message("No record found", "fail"); ?>
       </div>
     </div>
 <?php } ?>
