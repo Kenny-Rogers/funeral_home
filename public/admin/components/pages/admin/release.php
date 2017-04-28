cd628fcd53b1e202767776677f9420daa397d9c1
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 04:14:44 +0000

    including days spent in morgue

--- public/admin/components/pages/admin/release.php
+++ public/admin/components/pages/admin/release.php
@@ -1,13 +1,11 @@
 <?php
-  $deadbodies = Deadbody::find_all(" WHERE NOT status='released'");
+  $sql = "SELECT *, datediff(date_admitted, CURRENT_DATE) AS days_spent FROM"
+          ." deadbody WHERE NOT status='released'";
+  $deadbodies = Deadbody::find_by_sql($sql);
 ?>
 <div class="row">
         <div class="col-xs-12">
           <div class="box box-primary">
-            <!--div class="box-header">
-              <h3 class="box-title"></h3>
-            </div-->
-            <!-- /.box-header -->
             <div class="box-body">
               <table id="example2" class="table table-bordered table-hover">
                 <thead>
@@ -15,6 +13,7 @@
                   <th>Body Number</th>
                   <th>Name</th>
                   <th>Date of Death</th>
+                  <th>Days spent</th>
                   <th>Relative</th>
                   <th>Action</th>
                 </tr>
@@ -25,6 +24,7 @@
                     <td><?php echo $deadbody->id; ?></td>
                     <td><?php echo $deadbody->get_full_name(); ?></td>
                     <td><?php echo $deadbody->get_date_of_death(); ?></td>
+                    <td><?php echo $deadbody->get_days_spent();?></td>
                     <td>-</td>
                     <td><a href="?page=release_info&body_id=<?php echo $deadbody->id; ?>">Release</a></td>
                   </tr>
