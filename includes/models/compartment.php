f09d7b1e7cf65f50ae37401da56710f0e8f6a43c
Kenny-Rogers <ylodonu@gmail.com>
2017-04-28 02:11:31 +0000

    adding new fields

--- includes/models/compartment.php
+++ includes/models/compartment.php
@@ -4,10 +4,13 @@ require_once(LIB_PATH.DS.'database.php');
 class Compartment extends DatabaseObject {
   //this class interacts with the compartment table in the database
   protected static $table_name="compartment";
-  protected static $db_fields=array('id', 'description', 'suitable_for');
+  protected static $db_fields=array('id', 'description', 'suitable_for',
+                                    'status', 'dead_no');
   public $id;
   public $suitable_for;
   public $description;
+  public $status;
+  public $dead_no;
 
 }
 ?>
