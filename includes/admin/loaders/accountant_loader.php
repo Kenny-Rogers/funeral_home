107e5a191555c6cbbd3dc28a09686be52c2fe3f9
Kenny-Rogers <ylodonu@gmail.com>
2017-04-29 10:24:55 +0000

    adding page title and desc for all pages

--- includes/admin/loaders/accountant_loader.php
+++ includes/admin/loaders/accountant_loader.php
@@ -6,27 +6,37 @@ switch ($page) {
   case 'add_payment':
     $display =  ACCOUNTANT_PAGES.DS."add_payment.php";
     $class_ac = "active";
+    $page_title = "Add Payment";
+    $page_desc = "record payment for a specific body";
     break;
 
   case 'record_payment':
     $display =  ACCOUNTANT_PAGES.DS."record_payment.php";
     $class_ac = "active";
+    $page_title = "Record Payment";
+    $page_desc = "record payment for a specific body";
     break;
 
   case 'find_payment':
     $display =  GENERAL_PAGES.DS."find_info.php";
     $class_f = "active";
+    $page_title = "Find Payment";
+    $page_desc = "find all payments for a specific body";
     break;
 
   case 'display_info':
     $display =  GENERAL_PAGES.DS."display_info.php";
     $class_f = "active";
+    $page_title = "Find Payment";
+    $page_desc = "find all payments for a specific body";
     break;
 
 
   default:
     $display =  ACCOUNTANT_PAGES.DS."add_payment.php";
     $class_ac = "active";
+    $page_title = "Find Payment";
+    $page_desc = "find payment for a specific body";
     break;
 }
 ?>
