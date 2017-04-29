253f180e99ae971aa74b66865f602829400babf9
Kenny-Rogers <ylodonu@gmail.com>
2017-04-29 08:20:44 +0000

    adding new db fields

--- includes/models/requested_service.php
+++ includes/models/requested_service.php
@@ -4,12 +4,17 @@ require_once(LIB_PATH.DS.'database.php');
 class RequestedService extends DatabaseObject {
   //this class interacts with the relative table in the database
   protected static $table_name="requested_service";
-  protected static $db_fields=array('rel_no', 'dead_no',
-    'service_no', 'worker_no');
+  protected static $db_fields=array('id','rel_no', 'dead_no',
+    'service_no', 'worker_no', 'status');
   public $id;
   public $rel_no;
   public $dead_no;
   public $service_no;
   public $worker_no;
+  public $status;
+
+  // public function update(){
+  //   parent::update();
+  // }
 }
 ?>
