a4a2727a99e605c3d77f1e711cb955b8222acc48
ikeamofa <ikeamofa14@gmail.com>
2017-04-28 22:46:42 -0700

    created obituary page

---
+++ public/site1/pages/obituary.php
@@ -0,0 +1,105 @@
+<div class="container">
+
+    <div class="divPanel page-content">
+
+        <div class="breadcrumbs">
+                <a href="index.php">Home</a> &nbsp;/&nbsp; <span>Obituary</span>
+            </div>
+
+        <div class="row-fluid">
+			<!--Edit Main Content Area here-->
+                <div class="span8" id="divMain">
+
+                    <font color ="violet" size ="8"> List of obituaries</font>
+					<hr>
+
+                    <hr>
+
+
+					<font color ="violet" size = "5">Lloyd Frank Bitzer</font>
+
+			<div class="row-fluid">
+		        <div class="span2">
+                    <img src="images/lb.jpg" class="img-polaroid" style="margin:5px 0px 15px" alt="">   </div>
+                <div class="span10">
+                    <font face ="Agency FB" size ="4">1931-2016 <br>NCA President: 1976.
+                     <br/> <br/>
+                  </font>
+                </div>
+            </div>
+
+			<hr>
+
+			<hr>
+
+      <font color ="violet" size = "5">Lloyd Frank Bitzer</font>
+
+  <div class="row-fluid">
+        <div class="span2">
+                <img src="images/nc.jpg" class="img-polaroid" style="margin:5px 0px 25px" alt="">   </div>
+            <div class="span10">
+                <font face ="Agency FB" size ="4">1950 - 2015  <br> Grammy Award-winning singer and daughter of legendary jazz singer Nat "King" Cole.
+                 <br/> <br/>
+              </font>
+            </div>
+        </div>
+
+  <hr>
+
+  <hr>
+  <font color ="violet" size = "5">Bobbi Brown</font>
+
+<div class="row-fluid">
+    <div class="span2">
+            <img src="images/bb.jpg" class="img-polaroid" style="margin:5px 0px 15px" alt="">   </div>
+        <div class="span10">
+            <font face ="Agency FB" size ="4">1993-2015 <br>Daughter of singers Bobby Brown and the late Whitney Houston .
+             <br/> <br/>
+          </font>
+        </div>
+    </div>
+
+<hr>
+
+<hr>
+
+<font color ="violet" size = "5">James Best</font>
+
+<div class="row-fluid">
+  <div class="span2">
+          <img src="images/jb.jpg" class="img-polaroid" style="margin:5px 0px 15px" alt="">   </div>
+      <div class="span10">
+          <font face ="Agency FB" size ="4">1926-2015 <br> Prolific character actor best known for his role as Sheriff Rosco P. Coltrane on The Dukes of Hazzard .
+           <br/> <br/>
+        </font>
+      </div>
+  </div>
+
+<hr>
+
+<hr>
+<font color ="violet" size = "5">Dusty Rhodes</font>
+
+<div class="row-fluid">
+  <div class="span2">
+          <img src="images/dr.jpg" class="img-polaroid" style="margin:5px 0px 15px" alt="">   </div>
+      <div class="span10">
+          <font face ="Agency FB" size ="4">1945-2015 <br> former professional wrestler known by his fans as Dusty Rhodes .
+           <br/> <br/>
+        </font>
+      </div>
+  </div>
+
+<hr>
+
+<hr>
+
+
+          </div>
+
+            </div>
+
+        <div id="footerInnerSeparator"></div>
+    </div>
+
+</div>
