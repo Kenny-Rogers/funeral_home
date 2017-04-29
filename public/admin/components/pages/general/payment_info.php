1e0b3c7ebfafa97df68132d8c8e00a68ad02537a
Kenny-Rogers <ylodonu@gmail.com>
2017-04-29 09:41:32 +0000

    including payment table

--- public/admin/components/pages/general/payment_info.php
+++ public/admin/components/pages/general/payment_info.php
@@ -5,8 +5,48 @@
         </h4>
       </div>
       <div id="collapse4" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div class="panel-body">
+          <div class="row">
+                  <div class="col-xs-12">
+                    <?php if ($payments) { ?>
+                    <div class="box box-primary">
+                      <div class="box-header">
+                        <h3 class="box-title">All Payments For This Body</h3>
+                      </div>
+                      <!-- /.box-header -->
+                      <div class="box-body">
+                        <table id="example2" class="table table-bordered table-hover">
+                          <thead>
+                          <tr>
+                            <th>Payment Id</th>
+                            <th>Paid By</th>
+                            <th>Description</th>
+                            <th>Amount</th>
+                            <th>Date Paid</th>
+                          </tr>
+                          </thead>
+                          <tbody>
+                          <?php foreach ($payments as $payment) { ?>
+                            <tr>
+                            <td><?php echo $payment->id; ?></td>
+                            <td><?php echo $payment->paid_by; ?></td>
+                            <td><?php echo $payment->description; ?></td>
+                            <td><?php echo $payment->amount; ?></td>
+                            <td><?php echo datetime_to_text($payment->date_paid); ?></td>
+                          </tr>
+                          <?php } ?>
+                          </tbody>
+                        </table>
+                      </div>
+                      <!-- /.box-body -->
+                    </div>
+                    <!-- /.box -->
+                    <?php } else {
+                        //if no payment record found
+                        output_message("No Payment Made", "fail");
+                    } ?>
+          </div>
+          </div>
+        </div>
       </div>
 </div>
