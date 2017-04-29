3b46d38f17af2d70e3c0a7ac341a78e177375423
Kenny-Rogers <ylodonu@gmail.com>
2017-04-29 14:59:59 +0000

    adding page links

--- public/admin/relative/components/nav_bar.php
+++ public/admin/relative/components/nav_bar.php
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-static-top">
     <div class="container">
       <div class="navbar-header">
-        <a href="../../index2.html" class="navbar-brand"><b>Admin</b>LTE</a>
+        <a href="../../index2.html" class="navbar-brand"><b>Cross</b>Over</a>
         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
           <i class="fa fa-bars"></i>
         </button>
@@ -11,32 +11,24 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-          <li><a href="#">Link</a></li>
+          <!--li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+          <li><a href="#">Link</a></li-->
           <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Services<span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li class="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li class="divider"></li>
-              <li><a href="#">One more separated link</a></li>
+              <li><a href="?page=find_info"><i class="fa fa-search"></i> <span>Find Information</span></a></li>
+              <li><a href="?page=request_service"><i class="fa fa-plus"></i> <span>Request For Service</span></a></li>
+              <!--li><a href="#"><i class="fa fa-search"></i> <span>Find Payment Information</span></a></li-->
+
             </ul>
           </li>
         </ul>
-        <form class="navbar-form navbar-left" role="search">
-          <div class="form-group">
-            <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
-          </div>
-        </form>
       </div>
       <!-- /.navbar-collapse -->
       <!-- Navbar Right Menu -->
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
-          <li>  <a href="#" class="">Sign out</a> </li>
+          <li>  <a href="#" class=""><i class="fa fa-sign-out"></i><span>Sign out</span></a> </li>
         </ul>
       </div>
       <!-- /.navbar-custom-menu -->
