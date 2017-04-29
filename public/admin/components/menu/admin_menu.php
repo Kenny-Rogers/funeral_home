c12fae5529fdcf2cdcb5ca3e27fa22eb043f1996
Kenny-Rogers <ylodonu@gmail.com>
2017-04-29 10:38:53 +0000

    adding icons

--- public/admin/components/menu/admin_menu.php
+++ public/admin/components/menu/admin_menu.php
@@ -6,15 +6,15 @@
     <!-- Sidebar Menu -->
     <ul class="sidebar-menu">
       <li class="header">MORTURARY MANAGEMENT</li>
-      <li class="<?php echo $class_ac; ?>"><a href="?page=add_corpse"><i class="fa fa-link"></i> <span>Add Corpse</span></a></li>
-      <li class="<?php echo $class_f; ?>"><a href="?page=find_info"><i class="fa fa-link"></i> <span>Find Corpse Information</span></a></li>
-      <li class="<?php echo $class_r; ?>"><a href="?page=release"><i class="fa fa-link"></i> <span>Release Corpse</span></a></li>
-      <li class="<?php echo $class_n; ?>"><a href="?page=notify"><i class="fa fa-link"></i> <span>Notify Relative</span></a></li>
+      <li class="<?php echo $class_ac; ?>"><a href="?page=add_corpse"><i class="fa fa-plus"></i> <span>Add Corpse</span></a></li>
+      <li class="<?php echo $class_f; ?>"><a href="?page=find_info"><i class="fa fa-search"></i> <span>Find Corpse Information</span></a></li>
+      <li class="<?php echo $class_r; ?>"><a href="?page=release"><i class="fa fa-minus-square"></i> <span>Release Corpse</span></a></li>
+      <li class="<?php echo $class_n; ?>"><a href="?page=notify"><i class="fa fa-bell"></i> <span>Notify Relative</span></a></li>
 
       <li class="header">STAFF MANAGEMENT</li>
-      <li class="<?php echo $class_as; ?>"><a href="?page=add_staff"><i class="fa fa-link"></i> <span>Add Staff</span></a></li>
-      <li class="<?php echo $class_rs; ?>"><a href="?page=remove_staff"><i class="fa fa-link"></i> <span>Remove Staff</span></a></li>
-      <li class="<?php echo $class_sl; ?>"><a href="?page=staff_log"><i class="fa fa-link"></i> <span>Staff Logs</span></a></li>
+      <li class="<?php echo $class_as; ?>"><a href="?page=add_staff"><i class="fa fa-plus"></i> <span>Add Staff</span></a></li>
+      <li class="<?php echo $class_rs; ?>"><a href="?page=remove_staff"><i class="fa fa-minus-square"></i> <span>Remove Staff</span></a></li>
+      <li class="<?php echo $class_sl; ?>"><a href="?page=staff_log"><i class="fa fa-history"></i> <span>Staff Logs</span></a></li>
     </ul>
     <!-- /.sidebar-menu -->
   </section>
