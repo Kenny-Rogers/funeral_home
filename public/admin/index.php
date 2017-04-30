f924cb596b2d5e34c483f9327ff327067bcaaf27
ikeamofa <ikeamofa14@gmail.com>
2017-04-29 21:03:39 -0700

    added some comments

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,3 +1,6 @@
+<!-- Index file for application side  -->
+
+<!--Php code which is uded to initialize all variables present  -->
 <?php include("../../includes/admin/initialize.php");?>
 
 <?php if(!$session->is_logged_in()){
@@ -7,16 +10,22 @@
           $worker = Worker::find_by_id($worker_id);
       } ?>
 
+<!-- including a php file which enables pages of specific users of the application to be loaded   -->
 <?php include("../../includes/admin/user_loader.php");?>
 
+<!--  including a php file which contains the  head of the html file  -->
 <?php include("components/head.php"); ?>
 
+<!--  including a php file which contains the  navigation bar  -->
 <?php include("components/header.php"); ?>
 
+<!--  including a php file which contains a variable to select the different users and their specified page -->
 <?php //include("components/menu.php");
       include("{$menu}");
 ?>
 
+<!--  including a php file which contains the main content   -->
 <?php include("components/main_page.php"); ?>
 
+<!--  including a php file which contains the footer  -->
 <?php include("components/footer.php"); ?>
