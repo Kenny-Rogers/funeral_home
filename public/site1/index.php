64e7b7ed5a512f82ff05a8dde8e12c8cdca0d8fc
ikeamofa <ikeamofa14@gmail.com>
2017-04-29 21:03:17 -0700

    added some comments

--- public/site1/index.php
+++ public/site1/index.php
@@ -1,11 +1,20 @@
+	<!-- Index file for website -->
+
+
+	<!--Php code which is uded to initialize all variables present -->
 <?php include("../../includes/admin/initialize.php");?>
 
+<!-- including a php file which enables web pages to be loaded   -->
 <?php include("../../includes/public/page_loader.php"); ?>
 
+<!--  including a php file which contains the main header of the website  -->
 <?php include("components/header.php"); ?>
 
+<!--   including a php file which contains the main navigation bar of the website  -->
 <?php include("components/nav_bar.php"); ?>
 
+<!--   including a php file which contains a variable used to contain location of the various web pages  -->
 <?php include("{$display}"); ?>
 
+<!--    including a php file which contains the main footer of the website   -->
 <?php include("components/footer.php");?>
