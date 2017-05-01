ec52136ecd3c872957cb1707cb5608f5a8309f2b
Kenny-Rogers <ylodonu@gmail.com>
2017-05-01 03:13:14 +0000

    reformating the quote

--- public/admin/relative/components/pages/add_service.php
+++ public/admin/relative/components/pages/add_service.php
@@ -6,7 +6,7 @@
   $service->service_no = $_POST["service_no"];
   $service->dead_no = $_POST["dead_no"];
   $service->status = "TODO";
-  
+
   if ($service->save()) {
     redirect_to("../../index.php?page=request_service&stat=1");
   } else {
