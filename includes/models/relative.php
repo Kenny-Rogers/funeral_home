a5cdc0e5d0bf59872d4906452f3e0300beae9a74
Kenny-Rogers <ylodonu@gmail.com>
2017-05-01 03:06:57 +0000

    adding more fields

--- includes/models/relative.php
+++ includes/models/relative.php
@@ -5,12 +5,14 @@ class Relative extends DatabaseObject {
   //this class interacts with the relative table in the database
   protected static $table_name="relative";
   protected static $db_fields=array('id', 'full_name', 'address', 'telephone',
-    'email');
+    'email', 'dead_no', 'receipt_no');
   public $id;
   public $full_name;
   public $address;
   public $telephone;
   public $email;
+  public $receipt_no;
+  public $dead_no;
 
 
   public function full_name(){
