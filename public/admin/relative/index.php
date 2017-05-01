2e19e92a2564c33235392ed94947eba086f69142
Kenny-Rogers <ylodonu@gmail.com>
2017-05-01 02:36:20 +0000

    locking out un logged in users

--- public/admin/relative/index.php
+++ public/admin/relative/index.php
@@ -1,6 +1,13 @@
 <?php
   include("../../../includes/admin/initialize.php");
 
+  if (!$session->is_logged_in()) {
+      redirect_to('../../site1/index.php?page=login');
+  } else {
+      $relative_id = $session->worker_id;
+      $relative = Relative::find_by_id($relative_id);
+  }
+
   //including a page loader
   include("../../../includes/admin/loaders/relative_loader.php");
 
