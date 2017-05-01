763170c64df9b716d0343ab77a897746d1cdefe3
Kenny-Rogers <ylodonu@gmail.com>
2017-05-01 03:04:59 +0000

    saves requested service

---
+++ public/admin/relative/components/pages/add_service.php
@@ -0,0 +1,15 @@
+<?php
+  include("../../../../../includes/admin/initialize.php");
+
+  $service = new RequestedService();
+  $service->rel_no = $_POST["rel_no"];
+  $service->service_no = $_POST["service_no"];
+  $service->dead_no = $_POST["dead_no"];
+  $service->status = "TODO";
+  
+  if ($service->save()) {
+    redirect_to("../../index.php?page=request_service&stat=1");
+  } else {
+    redirect_to("../../index.php?page=request_service&stat=2");
+  }
+?>
